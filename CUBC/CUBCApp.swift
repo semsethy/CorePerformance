1bdeca42fb45ac0c88455648fe371ae32a260cb8
semsethy <sethyrisk@gmail.com>
2025-07-07 21:20:51 +0700

    Initial Commit

---
+++ CUBC/CUBCApp.swift
@@ -0,0 +1,17 @@
+//
+//  CUBCApp.swift
+//  CUBC
+//
+//  Created by JoshipTy on 7/7/25.
+//
+
+import SwiftUI
+
+@main
+struct CUBCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
